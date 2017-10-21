2271d5e7a1d5f5747bc3444299aa45e16dded02a
Kyrtap5 <Kyrtap5@users.noreply.github.com>
2017-10-21 12:44:38 +0200

    Fixed some bugs and added comments

--- src/de/kyrtap5/mvgscreen/Main.java
+++ src/de/kyrtap5/mvgscreen/Main.java
@@ -7,11 +7,13 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    //Parameters and default values
     public static String station;
-    public static int refreshInterval = 10000;
+    public static int refreshInterval = 6 * 1000;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        //Initialize and start stage
         Parent root = FXMLLoader.load(getClass().getResource("Screen.fxml"));
         primaryStage.setTitle("MVG Screen");
         primaryStage.setFullScreen(true);
@@ -19,23 +21,31 @@ public class Main extends Application {
         primaryStage.show();
     }
 
-
     public static void main(String[] args) {
+        //Console output
         System.out.println("===== MVG Screen v1.1.0 =====\n");
 
+        //Print usage when no parameters given
         if (args.length != 0) station = args[0];
         else printUsage();
 
+        //Check whether refresh interval was passed and save it
         if (args.length >= 2) {
             refreshInterval = Integer.parseInt(args[1]) * 1000;
         }
 
+        //Start output
         System.out.println("Starting screen for '" + station + "' with refresh interval " + refreshInterval / 1000 + "s.");
 
+        //Create Screen
         launch(args);
     }
 
+    /**
+     * Prints a message in the console which gives usage information and help
+     */
     private static void printUsage() {
+        //Print usage with information and help
         System.out.println("Example usage: java -jar mvgscreen.jar Ostbahnhof 5\n" +
                 "Use the first parameter to define a stop or station and the second one to set the refresh interval in seconds (if empty, the default value of 10s will be used).");
         System.exit(-1);
--- src/de/kyrtap5/mvgscreen/ScreenController.java
+++ src/de/kyrtap5/mvgscreen/ScreenController.java
@@ -18,56 +18,76 @@ public class ScreenController {
 
     private final MvgTicker ticker = new MvgTicker(Main.station);
 
+    /**
+     * Initializes the UI
+     */
     public void initialize() {
+        //Change label texts to application values and change background colors
         stopLabel.setText(ticker.getStation());
         mainPane.setBackground(new Background(new BackgroundFill(Color.MEDIUMBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
         topPane.setBackground(new Background(new BackgroundFill(Color.DARKBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
 
+        //Pull departure information from the parser for the first time and check for exceptions
         try {
             refreshData();
         } catch (Exception e) {
             printException(e);
         }
 
+        //Initialize thread for refreshing the departures
         Thread th = new Thread(new TickerTask());
         th.setDaemon(true);
         th.start();
     }
 
 
+    /**
+     * Task for refreshing the UI in a time interval given in the Main class
+     */
     public class TickerTask extends Task {
         @Override
         public Void call() {
-            int counter = 0;
+            //Run infinite loop which refreshes all data when the refresh interval is reached
             while (true) {
                 try {
+                    //Refresh time label to show the current time
                     Platform.runLater(() -> clockLabel.setText(DateHandler.formatDate(new Date(), "HH:mm")));
+                    //Wait for Main.refreshInterval seconds
                     Thread.sleep(Main.refreshInterval);
-                    if (++counter == 20) {
-                        Platform.runLater(() -> {
-                            try {
-                                refreshData();
-                            } catch (Exception e) {
-                                printException(e);
-                            }
-                        });
-                        counter = 0;
-                    }
+                    //Refresh UI by calling refreshData(), print Exception when failed
+                    Platform.runLater(() -> {
+                        try {
+                            refreshData();
+                        } catch (Exception e) {
+                            printException(e);
+                        }
+                    });
                 } catch (Exception ex) {
-                    ex.printStackTrace();
+                    printException(ex);
                 }
             }
         }
+
     }
 
+    /**
+     * Replaces all departure elements by recent ones
+     * @throws Exception when an error in the parser occurs, i.e. no connection could be estabilished
+     */
     private void refreshData() throws Exception {
+        //Replace all departure elements in the screen by new ones assigned by the parser
         while (mainPane.getChildren().size() != 1) mainPane.getChildren().remove(1);
         for (Departure d : ticker.getDepartures(true, true, true, true)) {
             mainPane.getChildren().add(new StopElement(d));
         }
     }
 
+    /**
+     * Prints exception to the user and exits application when called
+     * @param exception The Exception to be printed out
+     */
     private void printException(Exception exception) {
+        //Print error and exit application when something went wrong
         System.err.println("An error occured while executing the application: " + exception.toString());
         System.exit(-1);
     }
